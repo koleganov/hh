1c2b3776d04afaeb66a8d058bef43c777b274c01
Koleganov Kostya <taggz1030@gmail.com>
2025-12-09 22:55:13 +0500

    changes in the first version

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { Navbar, Footer } from './components/layout'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { CoursesPage } from './pages/CoursesPage'
-import { Instructors } from './pages/Instructors'
-import { Contact } from './pages/Contact'
-import { Cart } from './pages/Cart'
 import { Login } from './pages/Login'
 
 function App() {
@@ -17,9 +14,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/courses" element={<CoursesPage />} />
-          <Route path="/instructors" element={<Instructors />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
